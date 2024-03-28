4ddae597711c6d320d2b09a6d9020a74560a3080
sanman <sanman.mandlik@autohangaradvantage.com>
2024-03-28 17:27:30 +0530

    form integrated in all pages

---
+++ js/custom.js
@@ -0,0 +1,76 @@
+$(document).ready(function () {
+    // Smooth scroll to the anchor when the link is clicked
+    $('a[href^="#"]').on('click', function (event) {
+        event.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        // Scroll animation with a duration of 800 milliseconds (adjust as needed)
+        $('html, body').animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function () {
+            window.location.hash = target; // Update the URL hash
+        });
+    });
+
+});
+
+
+$("#suscribeForm").submit(function (e) {
+    e.preventDefault();
+    var action = $(this).attr("action");
+    var data = {};
+    $(this)
+        .serializeArray()
+        .map(function (x) {
+            data[x.name] = x.value;
+        });
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .done(function () {
+            alert("Thank you for writing to us. We value your feedback.");
+            location.href = document.referrer;
+            // window.location.reload();
+            // $('.success').addClass('is-active');
+        })
+        .fail(function () {
+            alert("Please try after sometime!");
+        });
+});
+
+$("#healthcareplan").submit(function (e) {
+    e.preventDefault();
+    var action = $(this).attr("action");
+    var data = {};
+    $(this)
+        .serializeArray()
+        .map(function (x) {
+            data[x.name] = x.value;
+        });
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: JSON.stringify(data),
+        contentType: "application/json",
+        headers: {
+            Accept: "application/json",
+        },
+    })
+        .done(function () {
+            alert("Thank you for writing to us. We value your feedback.");
+            location.href = document.referrer;
+            // window.location.reload();
+            // $('.success').addClass('is-active');
+        })
+        .fail(function () {
+            alert("Please try after sometime!");
+        });
+});
